e8c4e223ad9592429025b7efc9038e7cdbfaa55d
Bobby Shackleton <bobstershackleton@gmail.com>
2021-08-26 20:43:42 +0100

    Added Commands

---
+++ commands/misc/help.js
@@ -0,0 +1,25 @@
+const DiscordJS = require('discord.js')
+
+module.exports = {
+    category: 'Testing',
+    description: 'Replies with pong', // Required for slash commands
+    slash: false, // Create both a slash and legacy command
+    testOnly: false, // Only register a slash command for the testing guilds
+    callback: ({ client, message, interaction }) => {
+        const embed = new DiscordJS.MessageEmbed()
+        .setColor('RANDOM')
+	    .setTitle('Help Information')
+        .setDescription('Below is the command list')
+        .addField('Misc', '- .help\n- .ping\n- .uptime', true)
+        .addField('Info', '- .userinfo\n- .serverinfo', true)
+        .addField('Fun', '- .randomword\n- .8ball\n- .weather', true)
+        .addField('Maths', '- .add\n- .subtract\n- .multiply\n- .divide', true)
+		.setTimestamp(Date.now());
+
+      // message is provided only for a legacy command
+      if (message) {
+        message.reply({embeds: [embed]})
+        return
+      }
+    },
+  }
